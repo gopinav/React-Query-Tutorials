7de705bd07f3b5803e63d383e753e05fc3166d65
Vishwas <vishwas.g5@gmail.com>
2021-08-11 15:24:32 +0530

    query cache and stale time

--- react-query-demo/src/components/RQSuperHeroes.page.js
+++ react-query-demo/src/components/RQSuperHeroes.page.js
@@ -1,17 +1,21 @@
 import { useQuery } from 'react-query'
 import axios from 'axios'
 
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
+
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery(
+  const { isLoading, data, isError, error, isFetching } = useQuery(
     'super-heroes',
-    () => {
-      return axios.get('http://localhost:4000/superheroes')
-    },
+    fetchSuperHeroes,
     {
       cacheTime: 5000,
       staleTime: 10000
     }
   )
+
+  console.log({ isLoading, isFetching })
 
   if (isLoading) {
     return <h2>Loading...</h2>
