538e911d13094e17622bbb031523054a0a51e86d
Vishwas <vishwas.g5@gmail.com>
2021-08-11 16:38:20 +0530

    Revert to default staleTime

--- react-query-demo/src/components/RQSuperHeroes.page.js
+++ react-query-demo/src/components/RQSuperHeroes.page.js
@@ -8,11 +8,7 @@ const fetchSuperHeroes = () => {
 export const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching } = useQuery(
     'super-heroes',
-    fetchSuperHeroes,
-    {
-      cacheTime: 5000,
-      staleTime: 10000
-    }
+    fetchSuperHeroes
   )
 
   console.log({ isLoading, isFetching })
